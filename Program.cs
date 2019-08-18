8a8cee32262ab80cf6953d214451643932928855
Sergey <sergey.mindblast@mail.ru>
2019-08-18 15:26:30 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StressCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
